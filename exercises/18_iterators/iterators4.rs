e462f625551619a5c27f8aaf15e13ed5cd1aef83
watanabe-tsubasa <t.watanabe423@gmail.com>
2025-09-16 21:19:51 +0900

    0916

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,4 +1,5 @@
 fn factorial(num: u64) -> u64 {
+    (1..=num).product()
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
     // https://en.wikipedia.org/wiki/Factorial
